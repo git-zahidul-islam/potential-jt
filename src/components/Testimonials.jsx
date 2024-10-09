5f3024c6d94f1772fb5af49e4a56b033fe3f7e70
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-10-09 18:28:09 +0600

    deploy issue fix

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -15,7 +15,7 @@ const Testimonials = () => {
                     </div>
                     {/* text */}
                     <div>
-                    <p>"Lorem ipsum dolor sit amet consectetur. In enim cursus odio accumsan. Id leo urna velit neque mattis id tellus arcu condimentum. Augue dictum dolor elementum convallis dignissim malesuada commodo ultrices."</p>
+                    <p>Lorem ipsum dolor sit amet consectetur. In enim cursus odio accumsan. Id leo urna velit neque mattis id tellus arcu condimentum. Augue dictum dolor elementum convallis dignissim malesuada commodo ultrices.</p>
                     <p className="font-semibold">Name</p>
                     <p>CEO</p>
                     </div>
