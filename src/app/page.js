1b01b83ee52a034ef06b5c596b8bf098205457ad
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-10-09 08:43:51 +0600

    banner added

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,16 @@ const Navbar = dynamic(() => import('@/shared/Navbar'), {
   ssr: true,
   loading: () => <div className="h-[128px] bg-white/90 w-full animate-bounce duration-500"></div>,
 });
+const Banner = dynamic(() => import('@/components/Banner'),{
+  ssr: true,
+  loading: () => <div className="h-[100px] bg-green-500 w-full animate-bounce duration-700"></div>
+})
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
+      <Banner/>
       <h1>dhfohshfsifisifvgbsi</h1>
     </div>
   );
---
+++ src/components/Banner.jsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import bannerImage from "../assets/banner/bannerimg.png"
+// react icon
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
+const Banner = () => {
+    return (
+        <section className="md:w-[80%] w-full mx-auto md:px-0 px-2">
+            <div className="flex md:flex-row flex-col gap-2">
+            <div className="space-y-3 md:w-[50%] w-full">
+                <p className="md:text-2xl text-lg font-semibold text-[#000000]">Hi I am</p>
+                <div>
+                    <h4 className="md:text-3xl text-xl text-[#FD6F00]">Muhammad Umair</h4>
+                    <div className="md:text-8xl text-4xl font-bold text-[#000000]">
+                        <h1>UI & UX</h1>
+                        <h1 className="text-end">Designer</h1>
+                    </div>
+                </div>
+                <p className="md:text-xl text-lg font-normal pt-6">Lorem ipsum dolor sit amet consectetur. Tristique amet sed massa nibh lectus netus in. Aliquet donec morbi convallis pretium. Turpis tempus pharetra</p>
+                <button className="text-lg bg-[#FD6F00] px-5 py-2 text-white/90">Hire Me</button>
+            </div>
+
+
+            <div className="md:w-[50%] w-full">
+                <Image src={bannerImage} alt="banner image" width={420} height={510}></Image>
+                <div className="flex gap-2 items-center">
+                    <FaFacebook size={30}/>
+                    <FaTwitter size={30}/>
+                    <FaInstagram size={30}/>
+                    <FaLinkedin size={30}/>
+                </div>
+
+            </div>
+        </div>
+        </section>
+    );
+};
+
+export default Banner;
